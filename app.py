35ff8f2c27756f938164f8d62524b0c6eaa9e251
Daniel <dsalarcon3@soy.sena.edu.co>
2024-02-08 00:34:24 -0500

    second commit

--- app.py
+++ app.py
@@ -1,27 +1,43 @@
+from flask import Flask, render_template, Response
 import cv2
 from pyzbar.pyzbar import decode
 import time
 
-cap=cv2.VideoCapture(0)
-cap.set(3, 640)
-cap.set(4, 480)
-used_codes = []
+app = Flask(__name__)
 
-camera = True
-while camera == True:
-    success, frame = cap.read()
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-    for code in decode(frame):
-        if code.data.decode('utf-8') not in used_codes:
-            print('Approved. The item has been registered')
-            print(code.data.decode('utf-8'))
-            used_codes.append(code.data.decode('utf-8'))
-            time.sleep(5)
-        elif code.data.decode('utf-8') in used_codes:
-            print('Sorry, the item has been registered')
-            time.sleep(5)
-        else:
-            pass
+def gen_frames():
+    cap = cv2.VideoCapture(0)
+    cap.set(3, 640)
+    cap.set(4, 480)
+    used_codes = []
 
-    cv2.imshow('Testing-code-scan', frame)
-    cv2.waitKey(1)
+    while True:
+        success, frame = cap.read()
+
+        for code in decode(frame):
+            if code.data.decode('utf-8') not in used_codes:
+                print('Approved. The item has been registered')
+                print(code.data.decode('utf-8'))
+                used_codes.append(code.data.decode('utf-8'))
+                time.sleep(5)
+            elif code.data.decode('utf-8') in used_codes:
+                print('Sorry, the item has been registered')
+                time.sleep(5)
+            else:
+                pass
+
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(debug=True)
